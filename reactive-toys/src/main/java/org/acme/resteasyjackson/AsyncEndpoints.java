bbf86c72173c4cfeb50c1f92206e849b90f8e8ed
SavaAdi <50299766+SavaAdi@users.noreply.github.com>
2021-01-10 21:31:01 +0200

    Added silly mutiny failure handling example

--- reactive-toys/src/main/java/org/acme/resteasyjackson/AsyncEndpoints.java
+++ reactive-toys/src/main/java/org/acme/resteasyjackson/AsyncEndpoints.java
@@ -9,6 +9,7 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.MissingResourceException;
 import java.util.concurrent.CompletionStage;
 
 @Path("/")
@@ -28,12 +29,24 @@ public class AsyncEndpoints {
     @GET
     @Path("/{name:[a-zA-Z]*}/scramble-mutiny")
     public Uni<Response> getMutinyAnagram(final @PathParam("name") String name) {
-        var responseItem = new SomeItem();
-        responseItem.property = name;
-        System.out.println(responseItem);
-        return Uni.createFrom().item(name).onItem().transform(toScramble ->
-            Response.ok(responseItem).
-                    type(MediaType.APPLICATION_JSON).build()
-        );
+        System.out.println("Got this name: " + name);
+        return Uni.createFrom().item(name).onItem()
+                .transform(toScramble -> {
+                            if (toScramble.length() < 3) {
+                                throw new MissingResourceException("I", "don't", "care");
+                            } else if(toScramble.length() >= 10) {
+                                throw new IllegalArgumentException("Too many letters");
+                            }
+                            else {
+                                return Response.ok(name).
+                                        type(MediaType.TEXT_PLAIN).build();
+                            }
+                        })
+                .onFailure(MissingResourceException.class)
+                    .recoverWithItem(() -> Response.ok(name + " FAILEEED").
+                            type(MediaType.TEXT_PLAIN).build())
+                .onFailure(IllegalArgumentException.class)
+                    .recoverWithItem(() -> Response.ok(name + " FAILEEED IO").
+                            type(MediaType.TEXT_PLAIN).build());
     }
 }
